f9a80cee8a156d7b0bdec00842724cf3bbb586b8
Sharlin <sharlin.rahman91rgmail.com>
2022-04-01 11:32:02 +0200

    displayed questions in divs

--- main.js
+++ main.js
@@ -5,16 +5,56 @@ let getData = async (url) => {
     return json;
 };
 
+let questionsAndAnswers;
+
 let renderQuestions = async () => {
     let quiz = await getData("https://opentdb.com/api.php?amount=10&type=boolean");
-    quiz.results.forEach((quiz_question) => {
-    let {question, correct_answer, incorrect_answers} = quiz_question;
-    let questionDiv = document.createElement("div");
-    questionDiv.style.border = "1px solid black";
-    questionDiv.innerHTML = `<p>Question: ${question}</p><p>Correct anwser: ${correct_answer}</p>
-    <p>Incorrect anwser: ${incorrect_answers}</p>`;
-    document.body.append(questionDiv);
-    })};
+    questionsAndAnswers = quiz.results;
+    let answers;
+    let output = [];
+    let questionContainer = document.createElement("div");
+    questionsAndAnswers.forEach((quiz_question) => {
+    let {question} = quiz_question;
+    
+
+    answers = [];
+    
+
+    answers.push(
+        '<label>'
+            + '<input type="radio" name="question" value="true">'
+            + true + 
+        '</label>'
+    );
+
+    answers.push(
+        '<label>'
+            + '<input type="radio" name="question" value="false">'
+            + false + 
+        '</label>'
+    );
+
+    output.push(
+        `<div class="questions">  ${question}  </div>` +
+        `<div class="answers">`+ answers.join('') + `</div>`
+
+    );})
+
+    output.forEach((quizOutput) => {
+        let newQuestion = document.createElement("div");
+        newQuestion.innerHTML = quizOutput;
+        newQuestion.style.border = "1px solid black";
+        questionContainer.append(newQuestion);
+
+    })
+    document.body.append(questionContainer)};
 
 
 renderQuestions();
+
+
+
+
+
+
+
