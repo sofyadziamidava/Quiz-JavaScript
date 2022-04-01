9216c7832e49cce43d12cd490e3560cf577aabe5
Sofya Dziamidava <sofya.dziamidava@gmail.com>
2022-04-01 13:55:29 +0200

    added methods to display result

--- main.js
+++ main.js
@@ -6,30 +6,30 @@ let getData = async (url) => {
 };
 
 let questionsAndAnswers;
+let questionContainer;
 
 let renderQuestions = async () => {
     let quiz = await getData("https://opentdb.com/api.php?amount=10&type=boolean");
     questionsAndAnswers = quiz.results;
     let answers;
     let output = [];
-    let questionContainer = document.createElement("div");
-    questionsAndAnswers.forEach((quiz_question) => {
-    let {question} = quiz_question;
-    
-
+    questionContainer = document.getElementById("quiz");
+    questionContainer.innerHTML = "";
+    for(var i=0; i<questionsAndAnswers.length; i++){
+    let {question} = questionsAndAnswers[i];
+  
     answers = [];
     
-
     answers.push(
         '<label>'
-            + '<input type="radio" name="question" value="true">'
+            + '<input type="radio" name="question'+i+'" value="True">'
             + true + 
         '</label>'
     );
 
     answers.push(
         '<label>'
-            + '<input type="radio" name="question" value="false">'
+            + '<input type="radio" name="question'+i+'" value="False">'
             + false + 
         '</label>'
     );
@@ -38,7 +38,7 @@ let renderQuestions = async () => {
         `<div class="questions">  ${question}  </div>` +
         `<div class="answers">`+ answers.join('') + `</div>`
 
-    );})
+    );}
 
     output.forEach((quizOutput) => {
         let newQuestion = document.createElement("div");
@@ -47,14 +47,50 @@ let renderQuestions = async () => {
         questionContainer.append(newQuestion);
 
     })
-    document.body.append(questionContainer)};
+    document.body.append(questionContainer);
+};
 
 
-renderQuestions();
+let resultsContainer = document.getElementById("results");
+
+let renderResults = (questionsAndAnswers, questionContainer, resultsContainer) => {
+    var answerContainers = questionContainer.querySelectorAll('.answers');
+ 
+    var userAnswer = '';
+    var numCorrect = 0;
 
 
+    for(var i=0; i<questionsAndAnswers.length; i++){
+        userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+        let correctAnswer;
+
+        if(questionsAndAnswers[i].correct_answer){
+            correctAnswer = "true";
+        } else{
+            correctAnswer = "false";
+        }
+
+        console.log("User: " + userAnswer);
+        console.log("Correct: " + correctAnswer);
+        console.log("correct_answer: " + questionsAndAnswers[i].correct_answer)
+
+        if(userAnswer===questionsAndAnswers[i].correct_answer){
+			numCorrect++;
+			answerContainers[i].style.color = 'green';
+		}
+		else{
+			answerContainers[i].style.color = 'red';
+		}
+    }
+
+   resultsContainer.innerHTML = numCorrect + ' out of ' + questionsAndAnswers.length;
+   document.body.append(resultsContainer);
+
+}
 
 
+document.querySelector("#start").addEventListener("click", () =>{
+ renderQuestions();});
 
-
-
+ document.querySelector("#submit").addEventListener("click", () =>{
+    renderResults(questionsAndAnswers, questionContainer, resultsContainer);});
